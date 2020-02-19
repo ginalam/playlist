57ae5425bf3b49ccf7d2df52625b8322e6a5c7a4
ginalam <Lam.gina510@gmail.com>
2020-02-19 02:29:28 +0000

    Im done today

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
-var songName = ["Don't Wanna Cry", "Any Song", "Black Swan"];
+var songName;
+var artistName;
+var songLength; 
+var songLink;
+
+let songPlayList = [songName, artistName, songLength, songLink]
+
+
+
+// //= ["Don't Wanna Cry", "Any Song", "Black Swan"];
+// = ["Seventeen", "Zico","BTS"]; 
+// = [ "3:26", "4:08","3:21"]; 
+
  
-song.forEach(function(song){
+// song.forEach(function(song){
     
-})
+// })
